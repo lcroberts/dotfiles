c6b28dc36f160f3b773bd32e970838f694b2ab47
logan <logancroberts@outlook.com>
2023-04-27 22:30:13 -0400

    Add a script to make symbolic links automatically

---
+++ makesymlinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+files=(
+".zshrc"
+".mozilla"
+"Scripts"
+".local/share/fonts"
+".config/i3"
+".config/kitty"
+".config/nvim"
+".config/polybar"
+".config/ranger"
+".config/wofi"
+)
+
+repo_dir=/home/logan/GitClones/dotfiles/
+mkdir file_backups
+for file in "${files[@]}"; do
+  mv "/home/$USER/"$file file_backups
+  ln -s $repo_dir$file "/home/$USER/$file"
+done
+
+fc-cache
