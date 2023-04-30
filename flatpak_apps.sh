efd6ba19f7c319386b2f668409b5ca0d8b392051
logan <logancroberts@outlook.com>
2023-04-30 13:47:50 -0400

    Add flatpak install script

---
+++ flatpak_apps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+FLATPAK_APPS=(
+"org.mozilla.firefox"
+"com.discordapp.Discord"
+"com.spotify.Client"
+"com.valvesoftware.Steam"
+"net.davidotek.pupgui2"
+"org.videolan.VLC"
+"net.lutris.Lutris"
+"md.obsidian.Obsidian"
+)
+
+for flatpak_app in "${FLATPAK_APPS[@]}"; do
+	flatpak -y install flathub $flatpak_app
+done
+
