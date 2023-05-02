8c71695a8e774d2d9adebfa33c754bd398a048a0
logan <logancroberts@outlook.com>
2023-05-02 18:13:48 -0400

    Add neovim install to sericea setup

--- sericea_setup.sh
+++ sericea_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rpm-ostree install zsh exa distrobox kitty ranger onedrive btop
+rpm-ostree install zsh exa distrobox kitty ranger onedrive btop neovim
 ./flatpak_apps.sh
 # ./makesymlinks.sh
