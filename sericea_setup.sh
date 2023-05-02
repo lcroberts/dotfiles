4a8a03c10208dc047456fc25f49f3950af088216
logan <logancroberts@outlook.com>
2023-05-02 09:38:26 -0400

    Edit sericea setup script

--- sericea_setup.sh
+++ sericea_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rpm-ostree install zsh exa distrobox kitty ranger onedrive
+rpm-ostree install zsh exa distrobox kitty ranger onedrive btop
 ./flatpak_apps.sh
-./makesymlinks.sh
+# ./makesymlinks.sh
