c7c421bf39ea3d662a040a2e1f7abb52e75a0c66
logan <logancroberts@outlook.com>
2023-05-05 09:28:25 -0400

    Auto launch programs, move workspaces, and fix nvim config

---
+++ Scripts/moveworkspaces.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sleep 5
+swaymsg '[workspace="1"]' move output to output right
+swaymsg '[workspace=$ws2]' move output to output left
+swaymsg '[workspace=$ws3]' move output to output right
+swaymsg '[workspace=$ws4]' move output to output right
+swaymsg '[workspace=$ws10]' move output to output left
