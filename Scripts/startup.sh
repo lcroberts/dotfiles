fca1d357b820b2e7291f0b5ce759a6f3a1c814e8
logan <logancroberts@outlook.com>
2023-05-16 20:16:31 -0400

    Edit sway config and startup script

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -10,10 +10,10 @@ flatpak run com.spotify.Client 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run org.keepassxc.KeePassXC 2>&1 &
 kitty 2>&1 &
-mullvad 2>&1 &
 $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
 
 sleep 5
+mullvad 2>&1 &
 swaymsg '[workspace=1]' move workspace to output $right_display
 swaymsg '[workspace=2]' move workspace to output $left_display
 swaymsg '[workspace=3]' move workspace to output $right_display
