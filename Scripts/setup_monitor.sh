553710d074211bb88925d1701e45afc672297b69
logan <logancroberts@outlook.com>
2023-06-01 10:12:43 -0400

    Fix startup and and add gaming monitor scripts

---
+++ Scripts/setup_monitor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pkill kanshi
+kanshi -c /home/logan/.config/kanshi/games 2>&1 &
--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-kanshi &
+kanshi 2>&1 &
+~/.screenlayout/test.sh 2>&1 &
 
-right_display=DP-1
-left_display=DP-3
+main_display=DP-1
+extra_display=DP-3
 
 steam 2>&1 &
 flatpak run com.discordapp.Discord 2>&1 &
@@ -14,8 +15,8 @@ $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.App
 
 sleep 5
 mullvad 2>&1 &
-swaymsg '[workspace=1]' move workspace to output $right_display
-swaymsg '[workspace=2]' move workspace to output $left_display
-swaymsg '[workspace=3]' move workspace to output $right_display
-swaymsg '[workspace=4]' move workspace to output $right_display
-swaymsg '[workspace=10]' move workspace to output $left_display
+swaymsg '[workspace=1]' move workspace to output $extra_display
+swaymsg '[workspace=2]' move workspace to output $main_display
+swaymsg '[workspace=3]' move workspace to output $extra_display
+swaymsg '[workspace=4]' move workspace to output $extra_display
+swaymsg '[workspace=10]' move workspace to output $main_display
