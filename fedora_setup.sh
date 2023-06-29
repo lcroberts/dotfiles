a78274a909ea97b51d0393e5024a1aaa4da14229
logan <logancroberts@outlook.com>
2023-06-29 12:59:12 -0400

    Setup monitor scripts with fedora setup

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo dnf install fastfetch steam lutris winetricks yt-dlp gamemode goverlay
+sudo dnf install fastfetch steam lutris winetricks yt-dlp gamemode goverlay mangohud
 
 ./flatpak_apps.sh
 ./makesymlinks.sh
+
+ln -s $HOME/Projects/dotfiles/Scripts/setup_monitor.sh $HOME/.local/bin/setup_monitor
+ln -s $HOME/Projects/dotfiles/Scripts/reset_monitor.sh $HOME/.local/bin/reset_monitor
