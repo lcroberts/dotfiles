6b5afb6042986b41f61f869cb41f5681f11914f1
LoganCRoberts <LoganCRoberts@outlook.com>
2023-06-30 18:56:54 -0400

    More robust automatic installs

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo dnf install fastfetch steam lutris winetricks yt-dlp gamemode goverlay mangohud
+# General Packages
+sudo dnf install -y fastfetch yt-dlp zsh
+# Gaming Packages
+sudo dnf -y install steam lutris winetricks gamemode goverlay mangohud
+# Neovim Packages
+sudo dnf -y install neovim npm gcc lua ripgrep
+
+# Oh my zsh install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ./flatpak_apps.sh
 ./makesymlinks.sh
