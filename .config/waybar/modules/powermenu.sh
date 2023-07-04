ef03e3785d670b902b9b8fe3826de00c93230473
logan <logancroberts@outlook.com>
2023-07-04 09:29:21 -0400

    Remove name from powermenu icon

--- .config/waybar/modules/powermenu.sh
+++ .config/waybar/modules/powermenu.sh
@@ -4,4 +4,5 @@ me=`whoami`
 me_name=`getent passwd ${me} | cut -d ':' -f 5 | cut -d ',' -f 1`
 class=powermenu
 
-echo -e "{\"text\":\""$me_name"\", \"class\":\""$class"\"}"
+# echo -e "{\"text\":\""$me_name"\", \"class\":\""$class"\"}"
+echo -e "{\"text\":\"""\", \"class\":\""$class"\"}"
