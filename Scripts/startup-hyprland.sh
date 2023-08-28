ce2fcbf9a0d9aee5d8de37e86c7bf9f58bac99f8
logan <logancroberts@outlook.com>
2023-08-28 18:37:56 -0400

    Small changes to application launch script for hyprland

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -8,8 +8,8 @@ flatpak run org.mozilla.firefox 2>&1 &
 flatpak run org.keepassxc.KeePassXC 2>&1 &
 flatpak run com.discordapp.Discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
-steam 2>&1 &
 $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
+steam 2>&1 &
 
 sleep 20
 
