0f36de951df0331e66a5a1c4f97cdc694317846d
logan <logancroberts@outlook.com>
2023-08-29 19:55:58 -0400

    Add automatic rofi theme install

--- setup-scripts/hyprland-install-fedora.sh
+++ setup-scripts/hyprland-install-fedora.sh
@@ -2,7 +2,14 @@
 
 sudo dnf copr enable solopasha/hyprland
 
-sudo dnf install -y hyprland hyprland-contrib waybar-hyprland xdg-desktop-portal-hyprland swaybg swaylock rofi-wayland
+sudo dnf install -y hyprland hyprland-contrib waybar-hyprland xdg-desktop-portal-hyprland swaybg swaylock rofi-wayland papirus-icon-theme
 sudo dnf remove -y xdg-desktop-portal-wlr # Hyprland only wants wlr or hyprland portal installed.
 
+git clone https://github.com/Murzchnvok/rofi-collection /dev/shm/rofi-themes/
+mkdir -p ~/.local/share/rofi/themes
+cp -r /dev/shm/rofi-themes/* ~/.local/share/rofi/themes/
+find /dev/shm/rofi-themes -name \*.rasi -exec mv {} ~/.local/share/rofi/themes \;
+sudo rm -r /dev/shm/rofi-themes
+
 echo "Install GTK Lock through distrobox and copy the binary from /usr/bin to the ~/.local/bin/"
+echo "Run rofi-theme-selector to choose your rofi theme"
