9df8c9c9ca3ccd98d66d16b544e416c82d91d24e
logan <logancroberts@outlook.com>
2023-08-29 19:27:03 -0400

    update hyprland installer and use grimblast

--- setup-scripts/hyprland-install-fedora.sh
+++ setup-scripts/hyprland-install-fedora.sh
@@ -2,7 +2,7 @@
 
 sudo dnf copr enable solopasha/hyprland
 
-sudo dnf install -y hyprland grimshot waybar-hyprland xdg-desktop-portal-hyprland swaybg swaylock
+sudo dnf install -y hyprland hyprland-contrib waybar-hyprland xdg-desktop-portal-hyprland swaybg swaylock
 sudo dnf remove -y xdg-desktop-portal-wlr # Hyprland only wants wlr or hyprland portal installed.
 
 echo "Install GTK Lock through distrobox and copy the binary from /usr/bin to the ~/.local/bin/"
