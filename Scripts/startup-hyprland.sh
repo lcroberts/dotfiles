e578d722a63f810e11a591b3bcb5aa6e61d2eb02
logan <logancroberts@outlook.com>
2023-08-31 19:10:09 -0400

    Distrobox steam launch

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -9,7 +9,7 @@ flatpak run org.keepassxc.KeePassXC 2>&1 &
 flatpak run com.discordapp.Discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
-steam 2>&1 &
+distrobox-enter -n bazzite-arch -- steam 2>&1 &
 
 sleep 20
 
