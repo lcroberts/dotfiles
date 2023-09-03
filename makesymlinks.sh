d104f25ed16c61a28f735a7455fcf98cf2cb2e18
logan <logancroberts@outlook.com>
2023-09-03 17:35:28 -0400

    Finalize makesymlinks so that it works with existing configs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-# files=(
-# ".zshrc"
-# ".zshenv"
-# ".screenlayout"
-# "Scripts"
-# ".local/share/fonts"
-# "Pictures/RepoImages"
-# ".config/i3"
-# ".config/kitty"
-# ".config/nvim"
-# ".config/polybar"
-# ".config/ranger"
-# ".config/wofi"
-# ".config/sway"
-# ".config/waybar"
-# ".config/kanshi"
-# ".config/MangoHud"
-# ".config/btop"
-# ".config/fastfetch"
-# ".config/tmux"
-# ".config/fcitx5"
-# ".local/share/fcitx5/themes"
-# ".config/hypr"
-# )
-#
-# repo_dir=$HOME/Projects/dotfiles/
-# mkdir -p file-backups/.local/share
-# mkdir -p file-backups/.config
-# mkdir -p file-backups/.local/share/fcitx5
-#
-# for file in "${files[@]}"; do
-#   mv "$HOME/"$file "file-backups/"$file
-# done
+files=(
+".zshrc"
+".zshenv"
+".screenlayout"
+"Scripts"
+".config/btop"
+".config/fastfetch"
+".config/fcitx5"
+".config/hypr"
+".config/i3"
+".config/kanshi"
+".config/kitty"
+".config/MangoHud"
+".config/nvim/lua/custom"
+".config/polybar"
+".config/ranger"
+".config/rofi"
+".config/sway"
+".config/tmux"
+".config/waybar"
+".config/wofi"
+".local/share/fcitx5/themes"
+)
+
+mkdir -p file-backups/.local/share/fcitx5
+mkdir -p file-backups/.config
+
+for file in "${files[@]}"; do
+  mv "$HOME/"$file "file-backups/"$file
+done
 
 stow . --dotfiles -t $HOME/
