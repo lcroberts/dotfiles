d8a128efb77021a393f1099e9f1b37c2d5de8f45
Logan Roberts <logancroberts@outlook.com>
2023-09-07 10:30:47 -0400

    Attempt to fix formatting and move ftplugin to appropriate spot

--- .config/nvim/lua/custom/configs/conform.lua
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -1,3 +1,7 @@
+require("conform.formatters.yamlfix").env = {
+	args = { "-assume-filename", "$FILENAME", '-style="{BasedOn: llvm, IndentWidth: 4}"' },
+	arg = { "-assume-filename", "$FILENAME", '-style="{BasedOn: llvm, IndentWidth: 4}"' },
+}
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
