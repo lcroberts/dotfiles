e2df82a6061e540822b1e65bec55dd6c3c902a3d
Logan Roberts <logancroberts@outlook.com>
2023-09-07 10:35:14 -0400

    Work on formatting

--- .config/nvim/lua/custom/configs/conform.lua
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -1,6 +1,8 @@
-require("conform.formatters.yamlfix").env = {
-	args = { "-assume-filename", "$FILENAME", '-style="{BasedOn: llvm, IndentWidth: 4}"' },
-	arg = { "-assume-filename", "$FILENAME", '-style="{BasedOn: llvm, IndentWidth: 4}"' },
+require("conform.formatters.yamlfix").args = {
+	"-assume-filename",
+	"$FILENAME",
+	'-style="{BasedOn: llvm, IndentWidth: 4}"',
+	-- arg = { "-assume-filename", "$FILENAME", '-style="{BasedOn: llvm, IndentWidth: 4}"' },
 }
 require("conform").setup({
 	formatters_by_ft = {
