34792c5489ecad2c64b26d06550eaacc8963c529
lcroberts <logancroberts@outlook.com>
2023-10-14 13:12:13 -0400

    Switch off flatpak discord

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -6,7 +6,8 @@ extra_display=DP-3
 kitty tmux new -As main 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run org.keepassxc.KeePassXC 2>&1 &
-flatpak run com.discordapp.Discord 2>&1 &
+# flatpak run com.discordapp.Discord 2>&1 &
+discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 spotify 2>&1 &
 $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
@@ -14,6 +15,6 @@ steam 2>&1 &
 
 sleep 20
 
-systemctl restart --user xdg-desktop-portal
+# systemctl restart --user xdg-desktop-portal
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,firefox"
