050408d3e0707b8f48d09969a51ac10cf75c6d21
lcroberts <logancroberts@outlook.com>
2023-10-17 21:21:45 -0400

    Update sway config

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -6,12 +6,13 @@ main_display=DP-1
 extra_display=DP-3
 
 steam 2>&1 &
-flatpak run com.discordapp.Discord 2>&1 &
+# flatpak run com.discordapp.Discord 2>&1 &
+discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run org.keepassxc.KeePassXC 2>&1 &
 kitty tmux new-session -As main 2>&1 &
-$HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
+~/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
 
 sleep 5
 swaymsg '[workspace=1]' move workspace to output $extra_display
