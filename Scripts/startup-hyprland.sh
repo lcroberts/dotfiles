db994c4d02338eed5dd85220b33f21de7d5ef59c
lcroberts <logancroberts@outlook.com>
2023-10-23 09:34:26 -0400

    Scuffed fix for xdg-desktop-portal-gtk not starting properly

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -12,6 +12,7 @@ flatpak run com.spotify.Client 2>&1 &
 spotify 2>&1 &
 $HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
 steam 2>&1 &
+systemctl --user start xdg-desktop-portal-gtk.service
 
 sleep 20
 
