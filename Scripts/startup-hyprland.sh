a393b72d08278740fa31b5272576ed5942016b45
lcroberts <logancroberts@outlook.com>
2023-10-29 11:10:58 -0400

    Move a few autostarts to after sleep

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -10,12 +10,12 @@ flatpak run org.keepassxc.KeePassXC 2>&1 &
 discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 spotify 2>&1 &
-$HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
 steam 2>&1 &
-systemctl --user start xdg-desktop-portal-gtk.service
 
 sleep 20
 
+$HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
+systemctl --user start xdg-desktop-portal-gtk.service
 # systemctl restart --user xdg-desktop-portal
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,firefox"
