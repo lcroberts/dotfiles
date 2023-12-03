37a06144dd931a788c06ccb629bb07c1029de161
Logan Roberts <logancroberts@outlook.com>
2023-12-03 10:21:34 -0500

    Use protonhax to launch inside of a wine prefix instead of protontricks

--- Scripts/launch-ce.sh
+++ Scripts/launch-ce.sh
@@ -3,6 +3,7 @@
 # A wrapper script to launch cheat engine within a certain steam wine prefix.
 # To use it provide the steam appid as the only argument to the program. The
 # ce_executable path may need to be changed depending on what version of CE
-# you have intsalled.
+# you have intsalled. `protonhax init %command%` needs to be present in your
+# steam launch options as well.
 ce_executable="$HOME/.wine/drive_c/Program Files/Cheat Engine 7.5/cheatengine-x86_64.exe"
-protontricks-launch --appid $1 "$ce_executable"
+protonhax run $1 "$ce_executable"
