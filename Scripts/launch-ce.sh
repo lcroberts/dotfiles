fa2790fbf4a8a616d6e5ac55634652672ca580a7
Logan Roberts <logancroberts@outlook.com>
2023-12-03 11:06:07 -0500

    Add appid argument support to launch-ce.sh

--- Scripts/launch-ce.sh
+++ Scripts/launch-ce.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+shopt -s extglob
 
 # A wrapper script to launch cheat engine within a certain steam wine prefix.
 # To use it you need to provide an argument matching one of the values in the
 # case statement. It will automatically launch it within the wine prefix.
+# It is also possible to provide the steam app id as an argument as well.
 # The ce_executable path may need to be changed depending on what version of CE
 # you have intsalled. `protonhax init %command%` needs to be present in your
 # steam launch options as well.
@@ -18,8 +20,11 @@ case $1 in
     remnant2)
         app_id=1282100
         ;;
+    +([0-9]) ) # Match any number which should be a steam app id
+        app_id=$1
+        ;;
     *)
-        echo "Invalid argument. Please input one of the following programs:"
+        echo "Invalid argument. Please input one of the following programs, or input a valid steam app id:"
         echo "ds3 (Dark Souls III), dsr (Dark Souls Remastered), remnant2"
         exit
         ;;
