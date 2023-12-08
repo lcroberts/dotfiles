f7a4b6ac81c0c19f74276762154621b572ba6138
Logan Roberts <logancroberts@outlook.com>
2023-12-08 10:16:49 -0500

    Customize swaylock effects option

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -2,5 +2,6 @@
 
 image=~/Pictures/Wallpapers/currentbg
 # swaylock -i $image
-# swaylock-effects -i $image --clock
-gtklock -b $image
+swaylock -i $image --clock --indicator --line-color 1a1b26 --line-clear-color 1a1b26 --line-ver-color 1a1b26 --inside-color 292e42 --inside-clear-color 292e42 --inside-ver-color 292e42 --ring-color c0caf5 --ring-clear-color c0caf5  --ring-ver-color c0caf5 \
+    --line-wrong-color 1a1b26 --ring-wrong-color F28B82 --inside-wrong-color 292e42 --bs-hl-color F28B82 --key-hl-color 81C995 --separator-color 1a1b26 --text-color FDD633 --text-clear-color FDD633 --text-ver-color FDD633 --text-wrong-color F28B82
+# gtklock -b $image
