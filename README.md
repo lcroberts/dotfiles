dbe831ea9c59ac21df8d7c78aecbc4db09d63e34
Logan Roberts <logancroberts@outlook.com>
2023-12-13 19:15:11 -0500

    Update readme and finish nvim config move

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
+
 This repo contains dotfiles managed with stow. The command to make symlinks is the makesymlinks.sh
+
+My neovim configuration has been moved to a separate repository. You can find it [here.](https://github.com/lcroberts/nvim-config)
+
+
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 files=(
-".zshrc"
-".zshenv"
-".screenlayout"
-"Scripts"
-".config/btop"
-".config/fastfetch"
-".config/fcitx5"
-".config/hypr"
-".config/i3"
-".config/kanshi"
-".config/kitty"
-".config/MangoHud"
-".config/nvim/lua/custom"
-".config/polybar"
-".config/ranger"
-".config/rofi"
-".config/sway"
-".config/tmux"
-".config/waybar"
-".config/wofi"
-".local/share/fcitx5/themes"
+    ".zshrc"
+    ".zshenv"
+    ".screenlayout"
+    "Scripts"
+    ".config/btop"
+    ".config/fastfetch"
+    ".config/fcitx5"
+    ".config/hypr"
+    ".config/i3"
+    ".config/kanshi"
+    ".config/kitty"
+    ".config/MangoHud"
+    ".config/polybar"
+    ".config/ranger"
+    ".config/rofi"
+    ".config/sway"
+    ".config/tmux"
+    ".config/waybar"
+    ".config/wofi"
+    ".local/share/fcitx5/themes"
 )
 
 sudo rm -Ir file-backups
 mkdir -p file-backups/.local/share/fcitx5
-mkdir -p file-backups/.config/nvim/lua/
 
 for file in "${files[@]}"; do
-  mv "$HOME/"$file "file-backups/"$file
+    mv "$HOME/""$file" "file-backups/""$file"
 done
 
-stow . --dotfiles -t $HOME/
+stow . --dotfiles -t "$HOME"/
 
-sudo ln -sf $HOME/Scripts /usr/local/bin/Scripts
+sudo ln -sf "$HOME"/Scripts /usr/local/bin/Scripts
+
+
+
