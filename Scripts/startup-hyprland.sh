362a38a1ead54d2bd66cd3adf7e945e9cf406368
Logan Roberts <logancroberts@outlook.com>
2024-01-16 12:17:46 -0500

    Fix hyprland conf for laptop

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-~/.screenlayout/test.sh 2>&1 &
 
 kitty tmux new -As main 2>&1 &
-flatpak run org.mozilla.firefox 2>&1 &
-flatpak run org.keepassxc.KeePassXC 2>&1 &
-flatpak run com.discordapp.Discord 2>&1 &
-# discord 2>&1 &
-flatpak run com.spotify.Client 2>&1 &
-# spotify 2>&1 &
-steam 2>&1 &
+firefox 2>&1 &
+keepassxc 2>&1 &
 
 sleep 5
 
-corectrl --minimize-systray 2>&1 &
 onedrivegui 2>&1 &
 
 sleep 20
