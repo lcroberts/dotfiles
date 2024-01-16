6747c678d194ba50c0d0d67119130f38a9600e97
Logan Roberts <logancroberts@outlook.com>
2024-01-15 20:59:13 -0500

    Update hyprland config with fixes for steam and some bindings

--- Scripts/startup-hyprland.sh
+++ Scripts/startup-hyprland.sh
@@ -9,12 +9,13 @@ flatpak run com.discordapp.Discord 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 # spotify 2>&1 &
 steam 2>&1 &
+
+sleep 5
+
 corectrl --minimize-systray 2>&1 &
+onedrivegui 2>&1 &
 
 sleep 20
 
-$HOME/Applications/OneDriveGUI-1.0.2-x86_64_1b84c900adc54250fd543f66395bd957.AppImage 2>&1 &
-# systemctl restart --user xdg-desktop-portal
-# systemctl --user start xdg-desktop-portal-gtk.service
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,firefox"
