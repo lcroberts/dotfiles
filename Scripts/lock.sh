237d23da8500fde446fea2088add29228e290aa1
Logan Roberts <logancroberts@outlook.com>
2024-02-09 18:42:56 -0500

    Update lock.sh. Now use blurred screen image

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/bin/bash
 
-image=~/Pictures/Wallpapers/currentbg
+# image=~/Pictures/Wallpapers/currentbg
 # swaylock -i $image
 
 inside=1a1b26
@@ -9,7 +9,10 @@ wrong=F28B82
 ring=bb9af7
 line=292e42
 keypress=c0caf5
-swaylock -i $image --clock --indicator --line-color $line --line-clear-color $line --line-ver-color $line --inside-color $inside --inside-clear-color $inside --inside-ver-color $inside --ring-color $ring --ring-clear-color $ring  --ring-ver-color $ring \
-    --line-wrong-color $line --ring-wrong-color $wrong --inside-wrong-color $inside --bs-hl-color $wrong --key-hl-color $keypress --separator-color $line --text-color $text --text-clear-color $text --text-ver-color $text --text-wrong-color $wrong
-
-# gtklock -b $image
+swaylock --screenshots --clock --effect-blur 7x7 --indicator \
+    --line-color $line --line-clear-color $line --line-ver-color $line \
+    --inside-color $inside --inside-clear-color $inside --inside-ver-color $inside \
+    --ring-color $ring --ring-clear-color $ring  --ring-ver-color $ring \
+    --line-wrong-color $line --ring-wrong-color $wrong --inside-wrong-color $inside \
+    --bs-hl-color $wrong --key-hl-color $keypress --separator-color $line \
+    --text-color $text --text-clear-color $text --text-ver-color $text --text-wrong-color $wrong
