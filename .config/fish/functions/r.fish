70dc157d9a92b1fa74da7fbab18fcc198ff97325
Logan Roberts <logancroberts@outlook.com>
2024-02-14 19:10:53 -0500

    Move custom functions to config.fish

--- .config/fish/functions/r.fish
+++
@@ -1,3 +0,0 @@
-function r --wraps git --description "Navigate to git root"
-    cd $(git rev-parse --show-toplevel)
-end
--- .config/fish/functions/tmp.fish
+++
@@ -1,6 +0,0 @@
-function tmp --description "Make a temporary workspace"
-    set r "/tmp/workspaces/$(xxd -l3 -ps /dev/urandom)"
-    mkdir -p "$r"
-    echo "$r"
-    cd "$r"
-end
