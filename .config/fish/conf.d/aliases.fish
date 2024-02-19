2a54808d83941860a6041d69d81ee244046958ca
Logan Roberts <logancroberts@outlook.com>
2024-02-19 11:15:56 -0500

    Fix fish config to conform to desktop setup

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,12 +1,16 @@
-alias vim='nvim'
+abbr vim nvim
 # Archlinux update
-alias upgrade="paru -Syu"
+abbr upgrade paru -Syu
 # Alias to enter generic tmux session
-alias tenter="tmux new -As main"
+abbr tenter tmux new -As main
 # Hibernate alias
-alias hibernate="systemctl hibernate"
+abbr hibernate systemctl hibernate
 # rm interactive mode alias
-alias rm="rm -I"
+abbr rm rm -I
+# git aliases
+abbr gs git status
+abbr gl git log
+
 # tar multithreading
 alias tar="tar --use-compress-program=pigz"
 
@@ -21,18 +25,70 @@ alias llt="eza $eza_modifiers --tree -a"
 alias lx="eza $eza_modifiers -lbhHgmUa"
 
 # Movement aliases
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias .......='cd ../../../../../..'
-alias ........='cd ../../../../../../..'
-alias .........='cd ../../../../../../../..'
-alias ..........='cd ../../../../../../../../..'
-alias ...........='cd ../../../../../../../../../..'
-alias ............='cd ../../../../../../../../../../..'
-alias .............='cd ../../../../../../../../../../../..'
-alias ..............='cd ../../../../../../../../../../../../..'
-alias ...............='cd ../../../../../../../../../../../../../..'
-alias ................='cd ../../../../../../../../../../../../../../..'
+abbr - cd -
+abbr .. cd ..
+abbr ... cd ../..
+abbr .... cd ../../..
+abbr ..... cd ../../../..
+abbr ...... cd ../../../../..
+abbr ....... cd ../../../../../..
+abbr ........ cd ../../../../../../..
+abbr ......... cd ../../../../../../../..
+abbr .......... cd ../../../../../../../../..
+abbr ........... cd ../../../../../../../../../..
+abbr ............ cd ../../../../../../../../../../..
+abbr ............. cd ../../../../../../../../../../../..
+abbr .............. cd ../../../../../../../../../../../../..
+abbr ............... cd ../../../../../../../../../../../../../..
+abbr ................ cd ../../../../../../../../../../../../../../..
+
+function take --wraps mkdir --description 'Make directory and enter'
+    mkdir -p $argv && cd $argv
+end
+
+function j --wraps autojump --description 'cd autojump'
+    cd $(autojump $argv)
+end
+
+function pullall --description "Pull git repos within a subdirectory"
+    for dir in $(find . -maxdepth 5  -type d -name .git | sed 's|/.git$||')
+        cd $dir
+        set_color blue
+        echo "Entered $(pwd)"
+        set_color normal
+        git pull
+        set_color green
+        echo "Finished pull for repo in $dir"
+        set_color normal
+        cd -
+    end
+end
+
+function r --wraps git --description "Navigate to git root"
+    cd $(git rev-parse --show-toplevel)
+end
+
+function showme --description "Unzip provided archive into tmp directory"
+    if test -z $argv
+        echo "Pass file to show"
+        return 1
+    end
+    set tmp "/tmp/workspaces/$(xxd -l3 -ps /dev/urandom)"
+    mkdir -p "$tmp"
+    7z x "$argv" -o"$tmp"
+    cd "$tmp" || return 1
+    clear && echo && echo "$argv" && ls
+end
+
+function tmp --description "Make a temporary workspace"
+    set r "/tmp/workspaces/$(xxd -l3 -ps /dev/urandom)"
+    mkdir -p "$r"
+    echo "$r"
+    cd "$r"
+end
+
+function sd
+    set dirr $(find $argv | fzf-tmux -p 70%,75% --preview " [ -f {} ] && bat -f {} || eza -lah --color=always --tree {}")
+    [ -d $dirr ] && cd $dirr && return
+    [ -f $dirr ] && nvim $dirr && return
+end
