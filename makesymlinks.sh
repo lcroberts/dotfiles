972f5741b0980fb3edf49eab4616c4827f26f27f
Logan Roberts <logancroberts@outlook.com>
2024-03-14 12:50:32 -0400

    Add qtct theming stuff to configs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 files=(
-    ".zshrc"
-    ".zshenv"
-    ".screenlayout"
-    "Scripts"
-    ".config/btop"
-    ".config/fastfetch"
-    ".config/fcitx5"
-    ".config/hypr"
-    ".config/i3"
-    ".config/kanshi"
-    ".config/kitty"
-    ".config/MangoHud"
-    ".config/polybar"
-    ".config/ranger"
-    ".config/rofi"
-    ".config/sway"
-    ".config/tmux"
-    ".config/waybar"
-    ".config/wofi"
-    ".config/starship.toml"
-    ".config/fish"
-    ".local/share/fcitx5/themes"
+	".zshrc"
+	".zshenv"
+	".screenlayout"
+	"Scripts"
+	".config/btop"
+	".config/fastfetch"
+	".config/fcitx5"
+	".config/hypr"
+	".config/i3"
+	".config/kanshi"
+	".config/kitty"
+	".config/MangoHud"
+	".config/polybar"
+	".config/ranger"
+	".config/rofi"
+	".config/sway"
+	".config/tmux"
+	".config/waybar"
+	".config/wofi"
+	".config/qt5ct"
+	".config/qt6ct"
+	".config/starship.toml"
+	".config/fish"
+	".local/share/fcitx5/themes"
 )
 
 sudo rm -Ir file-backups
 mkdir -p file-backups/.local/share/fcitx5
 
 for file in "${files[@]}"; do
-    mv "$HOME/""$file" "file-backups/""$file"
+	mv "$HOME/""$file" "file-backups/""$file"
 done
 
 stow . --dotfiles -t "$HOME"/
