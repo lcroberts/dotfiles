2da76691232fcadab987f3088c4cb077890b8194
Logan Roberts <logancroberts@outlook.com>
2024-04-24 12:44:21 -0400

    Add detection so xwayland is only used on hyrpland

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,11 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 
 -- Disable wayland b/c, it doesn't work on hyprland
-config.enable_wayland = false
+if os.getenv("XDG_CURRENT_DESKTOP") == "Hyprland" then
+	config.enable_wayland = false
+else
+	config.enable_wayland = true
+end
 
 config.font = wezterm.font({
 	family = "JetBrainsMono NFM",
