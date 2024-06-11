39d66077dc9b1995817dd07bdfe034ea851a5430
Logan Roberts <logancroberts@outlook.com>
2024-06-11 15:31:40 -0400

    fish: fix vi cursor in insert mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    set fish_cursor_insert line
+
     bind -M insert \cn accept-autosuggestion
     bind -M insert \cr history-pager
     bind -M insert \e\u0020 "commandline --insert ' '" # alt+space for space without abbr expansion
