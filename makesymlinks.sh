78d4bb120e1d43352da8a1b153afc5f2af364c97
Logan Roberts <logancroberts@outlook.com>
2024-06-29 18:27:32 -0400

    Fix make symlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,50 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-files=(
-	".zshrc"
-	".zshenv"
-	".npmrc"
-	"Scripts"
-	".config/fastfetch"
-	".config/fcitx5"
-	".config/MangoHud"
-	".config/polybar"
-	".config/rofi"
-	".config/tmux"
-	".config/waybar"
-	".config/wofi"
-	".config/qt5ct"
-	".config/qt6ct"
-	".config/wezterm"
-	".config/starship.toml"
-	".config/fish"
-	".config/zathura"
-	".config/Kvantum"
-	".config/swaylock"
-	".config/swayidle"
-	".config/ags"
-	".config/kmonad"
-	".local/share/fcitx5"
-)
+cd ./stow/
 
-sudo rm -Ir file-backups
-mkdir -p file-backups/.local/share/fcitx5
-mkdir -p file-backups/.config
-
-for file in "${files[@]}"; do
-	if [ -L $file ]; then
-		rm $file
-	fi
-	if [ -d $file ]; then
-		mv "$HOME/""$file" "file-backups/""$file"
-	fi
-	if [ -f $file ]; then
-		mv "$HOME/""$file" "file-backups/""$file"
-	fi
+for dir in $(ls); do
+	stow --target=$HOME --adopt $dir
 done
-
-stow . --dotfiles -t "$HOME"/
-
-if [[ ! -L /usr/local/bin/Scripts ]]; then
-	sudo ln -sf "$HOME"/Scripts /usr/local/bin/Scripts
-fi
