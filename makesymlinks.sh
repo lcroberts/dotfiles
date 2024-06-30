fa0c9120a2450f33eab0a5d74a258e1d411235cc
Logan Roberts <logancroberts@outlook.com>
2024-06-30 18:55:52 -0400

    Update makesymlink.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 
-cd ./stow/
+symlink() {
+	cd $1
 
-for dir in $(ls); do
-	stow --target=$HOME --adopt $dir
-done
+	for dir in $(ls); do
+		stow --target=$HOME --adopt $dir
+	done
+}
+
+usage() {
+	echo "$0 [stow group]"
+	echo "Current available stow groups are: general, desktop, laptop"
+	echo ""
+	echo "To install only a single config you can run the following command in the directory where the config is located:"
+	echo "stow --target=\$HOME --adopt [config name]"
+}
+
+if [ ! $# -eq 1 ]; then
+	usage
+	exit 1
+fi
+
+case "$1" in
+"general")
+	symlink ./stow/
+	;;
+"desktop")
+	echo "desktop"
+	;;
+"laptop")
+	echo "laptop"
+	;;
+*)
+	usage
+	exit 1
+	;;
+esac
