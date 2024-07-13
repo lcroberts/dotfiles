41ed9f335028e188988c571d451f71a2033f390c
Logan Roberts <logancroberts@outlook.com>
2024-07-13 12:17:41 -0400

    Add ocaml env setup

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -7,6 +7,9 @@ if status is-interactive
     bind -M insert \cr history-pager
     bind -M insert \e\u0020 "commandline --insert ' '" # alt+space for space without abbr expansion
 
+    # opam configuration
+    source /var/home/logan/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
     direnv hook fish | source
     starship init fish | source
     zoxide init fish | source
