674a44c6cd5af3406f238c0af189586530626464
Logan Roberts <logancroberts@outlook.com>
2024-07-13 12:17:26 -0400

    Clean up setup stuff

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 symlink() {
-	cd $1
+    cd "$1" || exit
 
-	for dir in $(ls); do
-		stow --target=$HOME --adopt $dir
-	done
+    for dir in $(ls); do
+        stow --target="$HOME" --adopt "$dir"
+    done
 }
 
 usage() {
-	echo "$0 [stow group]"
-	echo "Current available stow groups are: general, desktop, laptop"
-	echo ""
-	echo "To install only a single config you can run the following command in the directory where the config is located:"
-	echo "stow --target=\$HOME --adopt [config name]"
+    echo "$0 [stow group]"
+    echo "Current available stow groups are: general, desktop, laptop"
+    echo ""
+    echo "To install only a single config you can run the following command in the directory where the config is located:"
+    echo "stow --target=\$HOME --adopt [config name]"
 }
 
 if [ ! $# -eq 1 ]; then
-	usage
-	exit 1
+    usage
+    exit 1
 fi
 
 case "$1" in
 "general")
-	symlink ./configs/
-	;;
+    symlink ./configs/
+    ;;
 "desktop")
-	symlink ./configs-desktop/
-	;;
+    symlink ./configs-desktop/
+    ;;
 "laptop")
-	symlink ./configs-laptop/
-	;;
+    symlink ./configs-laptop/
+    ;;
 *)
-	usage
-	exit 1
-	;;
+    usage
+    exit 1
+    ;;
 esac
