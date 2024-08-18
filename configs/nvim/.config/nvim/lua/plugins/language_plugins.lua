5df094c4c0b579f0192b8afce9baae8214673e7e
Logan Roberts <logancroberts@outlook.com>
2024-08-18 13:55:18 -0400

    Add latex editing stuff

--- configs/nvim/.config/nvim/lua/plugins/language_plugins.lua
+++ configs/nvim/.config/nvim/lua/plugins/language_plugins.lua
@@ -11,4 +11,20 @@ return {
     ft = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
     opts = {},
   },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 }
