b8d3ca939a22a24e8749675e3ef7c084e3aaba5e
Logan Roberts <logancroberts@outlook.com>
2024-08-18 19:09:49 -0400

    Add the ability to specify if the operations are on user or system flatpaks

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -1,32 +1,52 @@
 #!/usr/bin/env bash
 
 usage() {
-    echo "$0 [command]"
+    echo "$0 [command] OPTIONS"
     echo "Available commands are:"
     echo "  dump [filename]          Dumps all installed flatpak applications to a file"
     echo "  install [filename]       Installs all flatpak application listed in a dumped file"
     echo "  uninstall [filename]     Uninstalls all applications listed in a dumped file"
+    echo "Available options are:"
+    echo "  --user                   Performs operations on user flatpaks"
+    echo "  --system                 Performs operations on system flatpaks"
 }
 
-if [ -z ${2+x} ]; then
+# Checks that the second parameter is set
+if [[ -z ${2+x} ]]; then
     usage
     exit 1
+fi
+
+# Checks if third parameter is set and does checks if it is
+if [[ -n ${3+x} ]]; then
+    case "$3" in
+    "--user")
+        echo "Operating on user flatpaks."
+        ;;
+    "--system")
+        echo "Operating on system flatpaks."
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
 fi
 
 case "$1" in
 "dump")
     echo "Dumping flatpaks to $2..."
-    flatpak list --app | awk -F '\t' '{print $2}' >$2
+    flatpak list $3 --app | awk -F '\t' '{print $2}' >$2
     exit
     ;;
 "install")
     echo "Installing flatpaks in $2..."
-    flatpak install -- $(cat $2)
+    flatpak install $3 -- $(cat $2)
     exit
     ;;
 "uninstall")
     echo "Uninstalling flatpaks in $2..."
-    flatpak uninstall -- $(cat $2)
+    flatpak uninstall $3 -- $(cat $2)
     exit
     ;;
 *)
