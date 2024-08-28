71e67ca040480d540d4f35906d1e289086e44f0e
Logan Roberts <logancroberts@outlook.com>
2024-08-28 10:38:20 -0400

    yazi: get zoxide to work

--- configs/yazi/.config/yazi/plugins/smart-enter.yazi/init.lua
+++ configs/yazi/.config/yazi/plugins/smart-enter.yazi/init.lua
@@ -2,7 +2,8 @@ return {
 	entry = function()
 		local h = cx.active.current.hovered
 		if h.cha.is_dir then
-			-- os.execute("zoxide add " .. h.url)
+			local command = 'zoxide add "' .. tostring(h.url) .. '"'
+			os.execute(command)
 			ya.manager_emit("enter", { hovered = true })
 		else
 			ya.manager_emit("open", { hovered = true })
