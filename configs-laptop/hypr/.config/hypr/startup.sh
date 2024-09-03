093ae87634d12358b9a89e1c340ed5b46a16453f
Logan Roberts <logancroberts@outlook.com>
2024-09-03 17:57:02 -0400

    hypr(laptop): launch flatpak firefox

--- configs-laptop/hypr/.config/hypr/startup.sh
+++ configs-laptop/hypr/.config/hypr/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 kitty 2>&1 &
-flatpak run io.github.zen_browser.zen 2>&1 &
+flatpak run org.mozilla.firefox 2>&1 &
 sleep 10
 $HOME/AppImages/OneDriveGUI-1.0.3-x86_64.AppImage 2>&1 &
