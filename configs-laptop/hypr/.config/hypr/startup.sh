b164adf569f4b9b94f6a298fc8b821c47ef3eced
Logan Roberts <logancroberts@outlook.com>
2024-10-03 10:12:27 -0400

    hypr(laptop): move to new version of onedrivegui appimage

--- configs-laptop/hypr/.config/hypr/startup.sh
+++ configs-laptop/hypr/.config/hypr/startup.sh
@@ -3,6 +3,6 @@
 kitty 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 sleep 10
-$HOME/AppImages/OneDriveGUI-1.0.3-x86_64.AppImage 2>&1 &
+$HOME/AppImages/onedrivegui.appimage 2>&1 &
 
 hyprctl keyword windowrule "workspace unset,kitty"
