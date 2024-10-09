f5806bc75b26763eb3cdb4152debc1c3be36a897
Logan Roberts <logancroberts@outlook.com>
2024-10-09 10:13:56 -0400

    hypr(laptop): fix onedrive not launching on startup

--- configs-laptop/hypr/.config/hypr/startup.sh
+++ configs-laptop/hypr/.config/hypr/startup.sh
@@ -3,6 +3,6 @@
 kitty 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 sleep 10
-$HOME/AppImages/onedrivegui.appimage 2>&1 &
+$HOME/AppImages/onedrivegui*.appimage 2>&1 &
 
 hyprctl keyword windowrule "workspace unset,kitty"
