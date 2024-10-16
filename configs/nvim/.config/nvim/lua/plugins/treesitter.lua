baa4d702dc4f39a0939b712f17e0cbc3b891acd7
Logan Roberts <logancroberts@outlook.com>
2024-10-16 10:10:06 -0400

    nvim: Enable treesitter autoinstall

--- configs/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
 
       require('nvim-treesitter.configs').setup {
         -- ensure_installed = 'all',
+        auto_install = true,
         ensure_installed = {
           'bash',
           'c',
