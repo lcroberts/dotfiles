ce50abb9705e16b73a3c228552529590ddc1cd9c
Logan Roberts <logan@lcroberts.dev>
2025-06-07 18:50:17 -0400

    Update hyprland startup

--- dot_config/hypr/executable_startup.sh
+++ dot_config/hypr/executable_startup.sh
@@ -6,11 +6,11 @@ blueman-applet 2>&1 &
 ghostty 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
-flatpak run dev.vencord.Vesktop 2>&1 &
+flatpak run dev.vencord.Vesktop || distrobox-enter aur -- vesktop 2>&1 &
 
 sleep 10
 
-# find $HOME/Applications/ -iname "onedrive*.appimage" -exec {} \;
+find $HOME/Applications/ -iname "onedrive*.appimage" -exec {} \;
 
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,ghostty"
