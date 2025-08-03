3cf3e92ded954588af25f0677c7d3b4c2a4d7cd9
Logan Roberts <logan@lcroberts.dev>
2025-08-03 10:13:09 -0400

    Fix some path stuff

--- dot_config/zsh/zsh.d/01-path.zsh
+++ dot_config/zsh/zsh.d/01-path.zsh
@@ -1,14 +1,12 @@
+export GOPATH="$HOME/.local/share/go"
+export NPM_PACKAGES="$HOME/.npm-packages"
 # Add stuff to path with deduplication
 typeset -U path PATH # Make path dedupe values
 path+=~/.local/nvim/bin
-path+=~/.local/share/bob/nvim-bin
-path+=~/.local/share/go/bin
-path+=~/.npm-packages/bin
+path+="$GOPATH/bin"
+path+="$NPM_PACKAGES/bin"
 path+=~/.cargo/bin
-path+=~/.local/bin
-path+=~/Scripts
 path+=~/.composer/vendor/bin
 path+=/home/linuxbrew/.linuxbrew/bin
-path+=~/.cargo/bin
 path+=~/.local/bin
 path+=~/Scripts
