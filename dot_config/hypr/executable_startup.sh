a11f03b98116b6d64de547540d3a80934222ac05
Logan Roberts <logan@lcroberts.dev>
2025-12-26 21:43:17 -0500

    Fix startup with filen

--- dot_config/hypr/executable_startup.sh
+++ dot_config/hypr/executable_startup.sh
@@ -4,13 +4,12 @@ fcitx5 2>&1 &
 nm-applet 2>&1 &
 blueman-applet 2>&1 &
 kitty 2>&1 &
+@filendesktop 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
 flatpak run dev.vencord.Vesktop || distrobox-enter aur -- vesktop 2>&1 &
 
 sleep 10
-
-find "$HOME/Applications/" -iname "filen*.appimage" -exec {} \;
 
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,ghostty"
