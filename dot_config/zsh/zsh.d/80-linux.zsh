dd613947c28ae62f9cdec9597feca8d6fe0e99a9
Logan Roberts <logan@lcroberts.dev>
2025-12-28 13:58:06 -0500

    Fix frun

--- dot_config/zsh/zsh.d/80-linux.zsh
+++ dot_config/zsh/zsh.d/80-linux.zsh
@@ -3,6 +3,6 @@ alias firefox="flatpak run org.mozilla.firefox"
 alias protontricks="flatpak run com.github.Matoking.protontricks"
 alias protontricks-launch="flatpak run --command=protontricks-launch com.github.Matoking.protontricks"
 function frun() {
-    flatpak run (flatpak list --app | cut -f2 | fzf)
+    flatpak run $(flatpak list --app | cut -f2 | fzf)
 }
 
