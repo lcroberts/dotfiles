b3be1a0370bd3506c8ddbdff81420ef2395f585a
Logan Roberts <logan@lcroberts.dev>
2026-02-27 22:36:43 -0500

    Rework hyprland config

--- dot_config/hypr/executable_startup.sh
+++ dot_config/hypr/executable_startup.sh
@@ -4,12 +4,11 @@ fcitx5 2>&1 &
 nm-applet 2>&1 &
 blueman-applet 2>&1 &
 kitty 2>&1 &
-@filendesktop 2>&1 &
 flatpak run org.mozilla.firefox 2>&1 &
 flatpak run com.spotify.Client 2>&1 &
-flatpak run dev.vencord.Vesktop || distrobox-enter aur -- vesktop 2>&1 &
 
 sleep 10
 
+@filendesktop 2>&1 &
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,ghostty"
