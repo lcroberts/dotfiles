9312d70f557e4782c72724505fd5464c45d84ff2
Logan Roberts <logan@lcroberts.dev>
2026-04-10 19:23:31 -0400

    Prepend higher priority things topath

--- dot_config/zsh/zsh.d/01-path.zsh
+++ dot_config/zsh/zsh.d/01-path.zsh
@@ -2,11 +2,11 @@ export GOPATH="$HOME/.local/share/go"
 export NPM_PACKAGES="$HOME/.npm-packages"
 # Add stuff to path with deduplication
 typeset -U path PATH # Make path dedupe values
-path+=~/.local/nvim/bin
+path=("~/.local/nvim/bin" $path)
+path=("~/.local/bin" $path)
+path=("/home/linuxbrew/.linuxbrew/bin" $path)
 path+="$GOPATH/bin"
 path+="$NPM_PACKAGES/bin"
 path+=~/.cargo/bin
 path+=~/.composer/vendor/bin
-path+=/home/linuxbrew/.linuxbrew/bin
-path+=~/.local/bin
 path+=~/Scripts
