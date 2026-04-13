09628bdd46fad178b341013b9dc8a7821b9fb33e
Logan Roberts <logan@lcroberts.dev>
2026-04-12 20:09:16 -0400

    Fix path

--- dot_config/zsh/zsh.d/01-path.zsh
+++ dot_config/zsh/zsh.d/01-path.zsh
@@ -2,8 +2,8 @@ export GOPATH="$HOME/.local/share/go"
 export NPM_PACKAGES="$HOME/.npm-packages"
 # Add stuff to path with deduplication
 typeset -U path PATH # Make path dedupe values
-path=("~/.local/nvim/bin" $path)
-path=("~/.local/bin" $path)
+path=("$HOME/.local/neovim/bin" $path)
+path=("$HOME/.local/bin" $path)
 path=("/home/linuxbrew/.linuxbrew/bin" $path)
 path+="$GOPATH/bin"
 path+="$NPM_PACKAGES/bin"
